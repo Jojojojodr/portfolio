58f7f240479a10626b638c3e8460abf50b9a903c
Jordy de Ron <jordy@tronsoft.nl>
2025-10-02 00:56:58 +0200

    update css

--- frontend/components/blog_post_content_templ.go
+++ frontend/components/blog_post_content_templ.go
@@ -36,10 +36,6 @@ func BlogPostContent(c *gin.Context, post *models.BlogPost) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = markdownStyle().Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
 		if post != nil {
 			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<title hx-swap-oob=\"true\">")
 			if templ_7745c5c3_Err != nil {
@@ -48,7 +44,7 @@ func BlogPostContent(c *gin.Context, post *models.BlogPost) templ.Component {
 			var templ_7745c5c3_Var2 string
 			templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(post.Title)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `frontend/components/blog_post_content.templ`, Line: 13, Col: 46}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `frontend/components/blog_post_content.templ`, Line: 12, Col: 46}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 			if templ_7745c5c3_Err != nil {
@@ -61,7 +57,7 @@ func BlogPostContent(c *gin.Context, post *models.BlogPost) templ.Component {
 			var templ_7745c5c3_Var3 string
 			templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(post.Title)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `frontend/components/blog_post_content.templ`, Line: 14, Col: 56}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `frontend/components/blog_post_content.templ`, Line: 13, Col: 56}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 			if templ_7745c5c3_Err != nil {
@@ -74,7 +70,7 @@ func BlogPostContent(c *gin.Context, post *models.BlogPost) templ.Component {
 			var templ_7745c5c3_Var4 string
 			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(post.User.Name)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `frontend/components/blog_post_content.templ`, Line: 16, Col: 71}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `frontend/components/blog_post_content.templ`, Line: 15, Col: 71}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 			if templ_7745c5c3_Err != nil {
@@ -87,7 +83,7 @@ func BlogPostContent(c *gin.Context, post *models.BlogPost) templ.Component {
 			var templ_7745c5c3_Var5 string
 			templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(post.CreatedAt.Format("02-01-2006 15:04"))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `frontend/components/blog_post_content.templ`, Line: 17, Col: 66}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `frontend/components/blog_post_content.templ`, Line: 16, Col: 66}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 			if templ_7745c5c3_Err != nil {
@@ -136,7 +132,7 @@ func BlogPostContent(c *gin.Context, post *models.BlogPost) templ.Component {
 			var templ_7745c5c3_Var6 string
 			templ_7745c5c3_Var6, templ_7745c5c3_Err = templ.JoinStringErrs("/handle/blog/comments?post_id=" + fmt.Sprint(post.ID))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `frontend/components/blog_post_content.templ`, Line: 43, Col: 79}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `frontend/components/blog_post_content.templ`, Line: 42, Col: 79}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var6))
 			if templ_7745c5c3_Err != nil {
@@ -149,7 +145,7 @@ func BlogPostContent(c *gin.Context, post *models.BlogPost) templ.Component {
 			var templ_7745c5c3_Var7 string
 			templ_7745c5c3_Var7, templ_7745c5c3_Err = templ.JoinStringErrs("/handle/blog/comments/add?post_id=" + fmt.Sprint(post.ID))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `frontend/components/blog_post_content.templ`, Line: 49, Col: 84}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `frontend/components/blog_post_content.templ`, Line: 48, Col: 84}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var7))
 			if templ_7745c5c3_Err != nil {
@@ -164,35 +160,6 @@ func BlogPostContent(c *gin.Context, post *models.BlogPost) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-		}
-		return nil
-	})
-}
-
-func markdownStyle() templ.Component {
-	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
-		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
-		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
-			return templ_7745c5c3_CtxErr
-		}
-		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
-		if !templ_7745c5c3_IsBuffer {
-			defer func() {
-				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
-				if templ_7745c5c3_Err == nil {
-					templ_7745c5c3_Err = templ_7745c5c3_BufErr
-				}
-			}()
-		}
-		ctx = templ.InitializeContext(ctx)
-		templ_7745c5c3_Var8 := templ.GetChildren(ctx)
-		if templ_7745c5c3_Var8 == nil {
-			templ_7745c5c3_Var8 = templ.NopComponent
-		}
-		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 11, "<style>\n        .markdown h1 {\n            font-size: 2rem; /* 32px */\n            font-weight: bold;\n            margin-bottom: 1rem;\n            color: #5c3131;\n        }\n        .markdown h2 {\n            font-size: 1.5rem; /* 24px */\n            font-weight: bold;\n            margin-top: 1.5rem;\n            margin-bottom: 0.75rem;\n            color: #723f3f;\n        }\n        .markdown h3 {\n            font-size: 1.25rem; /* 20px */\n            font-weight: bold;\n            margin-top: 1.25rem;\n            margin-bottom: 0.5rem;\n            color: #8a4d4d;\n        }\n        .markdown h4 {\n            font-size: 1rem; /* 16px */\n            margin-top: 1rem;\n            margin-bottom: 0.5rem;\n            color: #a15b5b;\n        }\n        .markdown h5 {\n            font-size: 0.875rem; /* 14px */\n            margin-top: 0.875rem;\n            margin-bottom: 0.5rem;\n            color: #b16969;\n        }\n        .markdown h6 {\n            font-size: 0.75rem; /* 12px */\n            margin-top: 0.75rem;\n            margin-bottom: 0.5rem;\n            color: #c27777;\n        }\n        .markdown p {\n            font-size: 1rem; /* 16px */\n            line-height: 1.5;\n            color: #fff;\n        }\n        .markdown a {\n            color: #1e90ff; /* DodgerBlue */\n            text-decoration: underline;\n        }\n        .markdown a:hover {\n            text-decoration: none;\n        }\n    </style>")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
 		}
 		return nil
 	})
