f648471b25163da9ae214deba89093341eb17c27
Jordy de Ron <jordy@tronsoft.nl>
2026-09-13 06:44:04 +0200

    moved utils

---
+++ utils.go
@@ -0,0 +1,38 @@
+package portfolio
+
+import (
+	"fmt"
+
+	"github.com/Jojojojodr/portfolio/database/models"
+	
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+var SecretToken string
+
+func Encrypt(key string) string {
+	hashedKey, err := bcrypt.GenerateFromPassword([]byte(key), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Printf("Error encrypting key: %v\n", err)
+		return ""
+	}
+	return string(hashedKey)
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func IsDatabaseEmpty(database *gorm.DB) bool {
+    var userCount int64
+    
+    database.Model(&models.User{}).Count(&userCount)
+    
+    return userCount == 0
+}
+
+func SetSecretToken(token string) {
+	SecretToken = token
+}
